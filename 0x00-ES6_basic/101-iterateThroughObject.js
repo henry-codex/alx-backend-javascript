ce524862948aec98f01fcbff81c361deb71e0a46
henry-codex <httetteh@st.ug.edu.gh>
2023-11-28 05:22:00 +0000

    fixed error

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,10 +1,4 @@
-// 101. Iterate Through an Object with a for...of Statement
-  export default function iterateThroughObject(reportWithIterator) {
-    let string = '';
-  
-    for(const employee of reportWithIterator) {
-      string += employee + ' | '; 
-    }
-  
-    return string.slice(0, -3);
+// fixed
+export default function iterateThroughObject(reportWithIterator) {
+    return [...reportWithIterator].join(' | ');
   }
